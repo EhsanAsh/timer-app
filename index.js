12daac0d918fb06ea9fcd2526262837e910b642d
EhsanAsh <ehsan.ashrafipour@gmail.com>
2024-10-30 23:05:25 -0400

    Refactor Timer event callbacks to update SVG circle visualization

--- index.js
+++ index.js
@@ -1,72 +1,19 @@
-class Timer {
-	constructor(durationInput, startBtn, pauseBtn, callbacks) {
-		this.durationInput = durationInput;
-		this.startBtn = startBtn;
-		this.pauseBtn = pauseBtn;
-
-		if (callbacks) {
-			this.onStart = callbacks.onStart;
-			this.onTick = callbacks.onTick;
-			this.onComplete = callbacks.onComplete;
-		}
-
-		this.startBtn.addEventListener('click', this.start);
-		this.pauseBtn.addEventListener('click', this.pause);
-	}
-
-	/*
-  because we are adding an event listener to the start button to invoke the start method whenever it's been clicked, the value of 'this' will be the button not the new instance of the timer object. Which is not good. to fix this we can use arrow function.
-	behind the scenes it will be lifted up to the constructor. so if we apply console.log() above the constructor which is the first valid line, the value of 'this' in the start method and the console.log() above the constructor will be the new instance of the Timer object(since it's been lifted up to the constructor)
-	//*** another way is to use 'bind' inside the constructor like this:
-	this.startBtn.addEventListener('click', this.start.bind(this)); and then use regular function method. bind will return a new function which will not be executed immediately and force the value of this to be the Time object
-  */
-	start = () => {
-		if (this.onStart) {
-			this.onStart();
-		}
-		this.tick();
-		this.interval = setInterval(this.tick, 1000);
-	};
-
-	pause = () => {
-		clearInterval(this.interval);
-	};
-
-	tick = () => {
-		if (this.timeRemaining <= 0) {
-			this.pause();
-			if (this.onComplete) {
-				this.onComplete();
-			}
-		} else {
-			this.timeRemaining = this.timeRemaining - 1; // this.setter = this.getter - 1
-			if (this.onTick) {
-				this.onTick();
-			}
-		}
-	};
-
-	get timeRemaining() {
-		return parseFloat(this.durationInput.value);
-	}
-
-	set timeRemaining(time) {
-		this.durationInput.value = time;
-	}
-}
-
 const durationInput = document.getElementById('duration');
 const startBtn = document.getElementById('start');
 const pauseBtn = document.getElementById('pause');
-
+const circle = document.getElementsByTagName('circle')[0];
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+let duration;
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
-	onStart() {
-		console.log('Timer Started');
+	onStart(totalDuration) {
+		duration = totalDuration;
 	},
-	onTick() {
-		console.log('Timer Ticked down');
+	onTick(timeRemaining) {
+		circle.setAttribute(
+			'stroke-dashoffset',
+			(perimeter * timeRemaining) / duration - perimeter
+		);
 	},
-	onComplete() {
-		console.log('Timer Finished');
-	},
+	onComplete() {},
 });
